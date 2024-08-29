2f332e5baba6c6df10f76392d71d1f84f27079a8
Abdelquoddouss <fadliabdelquoddouss.06@gmail.com>
2024-08-29 17:22:01 +0100

    ajout de classe user

---
+++ src/entities/Utilisateur.java
@@ -0,0 +1,50 @@
+package entities;
+
+import java.util.List;
+
+public class Utilisateur {
+    private long id;
+    private String name;
+    private int age;
+    private List<CarbonConsommation> consommation;
+
+
+    public Utilisateur(long id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.consommation = consommation;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public List<CarbonConsommation> getConsommation() {
+        return consommation;
+    }
+
+    public void setConsommation(List<CarbonConsommation> consommation) {
+        this.consommation = consommation;
+    }
+}
