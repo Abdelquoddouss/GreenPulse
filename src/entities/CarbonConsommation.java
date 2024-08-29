99ebbbcd7b374f8fcd02090dc34100243cbdbbd4
Abdelquoddouss <fadliabdelquoddouss.06@gmail.com>
2024-08-29 22:18:14 +0100

    ajout de classe CarbonConsommation

--- src/entities/CarbonConsommation.java
+++ src/entities/CarbonConsommation.java
@@ -1,4 +1,30 @@
 package entities;
 
+import java.time.LocalDate;
+
 public class CarbonConsommation {
+    private LocalDate date;
+    private double carbonAmount;
+
+    public CarbonConsommation(LocalDate date, double carbonAmount) {
+        this.date = date;
+        this.carbonAmount = carbonAmount;
+    }
+
+    // Getters et setters
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public double getCarbonAmount() {
+        return carbonAmount;
+    }
+
+    public void setCarbonAmount(double carbonAmount) {
+        this.carbonAmount = carbonAmount;
+    }
 }
