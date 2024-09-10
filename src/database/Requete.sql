589f914482dde24f19e5f0d98b934e312d2ad427
Abdelquoddouss <fadliabdelquoddouss.06@gmail.com>
2024-09-10 09:58:47 +0100

    ajout des classe de consommation

--- src/database/Requete.sql
+++ src/database/Requete.sql
@@ -18,3 +18,29 @@ CREATE TABLE consommations (
 );
 
 
+CREATE TABLE transport (
+                           id SERIAL PRIMARY KEY,
+                           consommation_id INT ,
+                           distance_parcourue DOUBLE PRECISION,
+                           type_vehicule VARCHAR(50) ,
+                           FOREIGN KEY (consommation_id) REFERENCES consommations(id) ON DELETE CASCADE
+);
+
+CREATE TABLE logement (
+                          id SERIAL PRIMARY KEY,
+                          consommation_id INT ,
+                          consommation_energie DOUBLE PRECISION,
+                          type_energie VARCHAR(50) ,
+                          FOREIGN KEY (consommation_id) REFERENCES consommations(id) ON DELETE CASCADE
+);
+
+CREATE TABLE alimentation (
+                              id SERIAL PRIMARY KEY,
+                              consommation_id INT ,
+                              type_aliment VARCHAR(50) ,
+                              poids DOUBLE PRECISION,
+                              FOREIGN KEY (consommation_id) REFERENCES consommations(id) ON DELETE CASCADE
+);
+
+
+CREATE TYPE consommation_type AS ENUM ('transport', 'logement', 'alimentation');
---
+++ src/service/GestionConsommation.java
@@ -0,0 +1,14 @@
+package service;
+
+import java.sql.Connection;
+
+public class GestionConsommation {
+    private Connection connection;
+
+    public GestionConsommation(Connection connection) {
+        this.connection= connection;
+    }
+
+
+
+}
